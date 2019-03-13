66edb7a23afc798df9559d509829d822e773807b
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-03-13 14:50:27 -0500

    Project created and Automatic Reference Counting disabled

---
+++ Contacts MRC/Contacts MRC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts MRC
+//
+//  Created by Audrey Welch on 3/13/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
